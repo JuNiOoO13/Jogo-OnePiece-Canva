afb5c0118a8f8d493aafa3e8b5ee687dfa86fd82
junior <junioroooppp@gmail.com>
2024-09-28 20:45:33 -0300

    adicao de funçoes

--- Scripts/char.js
+++ Scripts/char.js
@@ -11,58 +11,90 @@ export default class Char{
         this.posY = 0;
         this.loadFinish = false;
         this.sprites = this._loadImage(spritesUrl);
+        this.isWalking = false;
+        this.attacking = false;
+        this.frame = 0;
     }
     _loadAllAnimations(configsAnim){
         let keys = Object.keys(configsAnim);
         keys.forEach((key) => {
-            configsAnim[key]['frames'] = this._loadAnimation(configsAnim[key],key[key.length-1])
+            if(!configsAnim[key].hasOwnProperty('frames'))
+                configsAnim[key]['frames'] = this._loadAnimation(configsAnim[key])
         });
         return configsAnim;
     }   
-    _loadAnimation(configAnim,direction){
+    _loadAnimation(configAnim){
         let frames = [];
         let posX = configAnim.StartPosX;
         let posY = configAnim.StartPosY;
         for(let i = 0; i < configAnim.QuantFrames;i++){
             frames.push([posX,posY]);
-            if(direction == "L"){
-                posX -= (configAnim.Width + configAnim.SpaceBetween);
-            }else{
                 posX += (configAnim.Width + configAnim.SpaceBetween);
-            }
         }
         return frames;
     }
     _loadImage(spritesUrl){
         this.screen.imageSmoothingEnabled = false;
 
-        // Ou para garantir compatibilidade com todos os navegadores:
-        this.screen.webkitImageSmoothingEnabled = false;
-        this.screen.mozImageSmoothingEnabled = false;
-        this.screen.msImageSmoothingEnabled = false;
-        this.screen.imageSmoothingEnabled = false; 
         let sprites = new Image();
         sprites.src = spritesUrl
         sprites.onload = () => {
-            console.log(this.posY)
-            this.screen.drawImage(this.sprites,
-                this.anim[this.currentAction].frames[this.currentSprite][0],
-                this.anim[this.currentAction].frames[this.currentSprite][1],
-                this.anim[this.currentAction].Width,
-                this.anim[this.currentAction].Height,
-                this.posX,
-                this.posY,
-                this.anim[this.currentAction].Width,
-                this.anim[this.currentAction].Height
-
-            )
             this.loadFinish = true;
         }
         return sprites;
     }
     
+    _animate(){
+        this.screen.drawImage(this.sprites,
+            this.anim[this.currentAction].frames[this.currentSprite][0],
+            this.anim[this.currentAction].frames[this.currentSprite][1],
+            this.anim[this.currentAction].Width,
+            this.anim[this.currentAction].Height,
+            this.posX,
+            this.posY,
+            this.anim[this.currentAction].Width * 2.5, 
+            this.anim[this.currentAction].Height * 2.5
+        )
+        this.currentSprite = Math.floor(this.frame  / (59 / this.anim[this.currentAction].QuantFrames));
+        if(this.currentSprite > this.anim[this.currentAction].QuantFrames - 1){
+            this.currentSprite = 0;
+            if(this.attacking){
+                this.attacking = false;
+                this.idle()
+            }
+        }
+        this.frame++;
+        if(this.frame >= 60){
+            this.frame = 0;
+        }
+    }
+
+    walk(direction){
+        if(!this.isWalking){
+            this.isWalking = true;
+            this.direction = direction
+            this.currentAction = `Walk${direction}`;
+            this.currentSprite = 0;
+        }
+        
+    }
+
+    idle(){
+        if(!this.attacking){
+            this.currentAction = `Stand${this.direction}`;
+            this.currentSprite = 0;
+            this.isWalking = false;
+        }
+    }
+    attack1(){
+        this.currentAction = `Attack1${this.direction}`;
+        this.currentSprite = 0;
+        this.isWalking = false;
+        this.attacking = true;
+    }
 
     draw(){
-        
+        if(this.loadFinish)
+           this._animate();
     }
 }
--- Scripts/main.js
+++ Scripts/main.js
@@ -5,19 +5,61 @@ function _createCtx(){
     let canva = document.getElementById('canva');
     return canva.getContext('2d');
 }
+
+
+
 let anim = await utilits.loadCharConfigs("Zoro")
 let ctx = _createCtx();
+let velocity = 0;
+let inputsPlayer = {
+    'd' : () =>{
+        mainChar.walk('R');
+        velocity = 4;
+    },
+    'a' : () => {
+        mainChar.walk('L');
+        velocity = -4;
+    },
+    'f': () => {
+        if(!mainChar.attacking)
+            mainChar.attack1();
+    }
+}
+let keys = Object.keys(inputsPlayer)
 ctx.imageSmoothingEnabled = false;
-ctx.imageSmoothingEnabled = false; // Desativa a suavização
-ctx.mozImageSmoothingEnabled = false; // Compatibilidade com Firefox
-ctx.webkitImageSmoothingEnabled = false; // Compatibilidade com Webkit
-ctx.msImageSmoothingEnabled = false; // Compatibilidade com IE
+ctx.imageSmoothingEnabled = false;
+ctx.mozImageSmoothingEnabled = false; 
+ctx.webkitImageSmoothingEnabled = false; 
+ctx.msImageSmoothingEnabled = false; 
 
 let mainChar = new Char("Zoro", "/Assets/ZoroSprites.png", anim,"R",ctx);
+mainChar.posY = canva.height / 2 - 100
 
 
+function mainLoop(){
+    ctx.clearRect(0,0,canva.width,canva.height);    
+    mainChar.draw();
+    mainChar.posX += velocity
+}
+
+
+
+document.addEventListener('keydown', (event) => {
+    if(keys.includes(event.key)){
+        inputsPlayer[event.key]()
+    }
+})
+
+document.addEventListener('keyup', (event) =>{
+    if(event.key == 'd' || event.key == 'a'){
+        mainChar.idle();
+        velocity = 0;
+    }
+});
+// setInterval(() => {
+//     mainLoopLogics()
+//  },250);
 
 setInterval(() => {
-    mainChar.draw()
-    
-},200);
+   mainLoop()
+},16.6);
